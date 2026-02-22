2274822b11644a7bf43d8c68921d864b821e4b1f
Djangodev2010 <gaminglz507@gmail.com>
2026-02-22 12:12:00 +0530

    Created new context_processory to get all the categories dynamically. Created 404.html for displaying a custom 404 page. Created base.html to follow DRY principle in my templates. Updated home-blogs.html and posts_by_category.html for template inheritance. Updated settings.py to include the custom context_processor.

---
+++ blog/blog_app/context_processors.py
@@ -0,0 +1,5 @@
+from blog_app.models import Category
+
+def get_categories(request):
+    categories = Category.objects.all()
+    return dict(categories=categories)
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -6,15 +6,13 @@ from django.http import Http404
 
 def home(request):
     # Display all the categories on the homepage and the latest featured posts and regular posts
-    categories = Category.objects.all()
     featured_post = Blog.objects.filter(is_featured=True).order_by('-updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published').order_by('-updated_at')
-    return render(request, 'home-blogs.html', context={'categories': categories, 'featured_posts': featured_post, 'posts': posts})
+    return render(request, 'home-blogs.html', context={'featured_posts': featured_post, 'posts': posts})
 
 def posts_by_category(request, pk):
     # Fetch the category from the database
-    category = Category.objects.get(Category, id=pk)
-    categories = Category.objects.all()
+    category = get_object_or_404(Category, id=pk)
     # Fetch the posts that have the same category
     posts = category.blogs.filter(status='Published').order_by('-updated_at')
-    return render(request, 'posts_by_category.html', context={'posts': posts, 'categories': categories, 'category': category})
+    return render(request, 'posts_by_category.html', context={'posts': posts, 'category': category})
